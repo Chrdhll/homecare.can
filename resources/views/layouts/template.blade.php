bef07a4d98e9d604fea5bc61a57249e12d5ed285
Fadhil <m.fadil3434@gmail.com>
2025-11-16 10:59:30 +0700

    container navbar

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -39,7 +39,7 @@
 
 <body class="index-page">
     <header id="header" class="header d-flex align-items-center fixed-top">
-        <div class="container-fluid container-xl position-relative d-flex align-items-center">
+        <div class="container container-xl position-relative d-flex align-items-center">
             <a href="{{ route('home') }}" class="logo d-flex align-items-center me-auto">
                 <!-- Uncomment the line below if you also wish to use an image logo -->
                 <img src="{{ asset('assets/img/logo_1.png') }}" alt="" />
